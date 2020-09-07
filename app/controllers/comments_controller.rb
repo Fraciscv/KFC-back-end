1416093d535a7b8e53d7c16860a561d798c4060e
kceron <karemiceron@gmail.com>
2020-09-07 14:43:39 -0400

    seeds

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,10 @@
 class CommentsController < ApplicationController
+
+    def index 
+        render json: Comment.all
+    end
+
+    def show 
+        render json: Comment.find(params[:id])
+    end 
 end
