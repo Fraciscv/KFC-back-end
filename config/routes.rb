c1221601c80311898c428c1cca1bfc745a385af7
kceron <karemiceron@gmail.com>
2020-09-10 18:24:10 -0400

    EW

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :events
   resources :users
 
+  get '/getbirthdatecontent', to: 'events#getbirthdatecontent'
   post '/getbirthdatecontent', to: 'events#getbirthdatecontent'
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,23 +7,6 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 # seed data ###################################
-User.destroy_all
-Event.destroy_all
-# USERS
-fran = User.create({name: "Fran", birthdate: "02-02-1993"})
-barbs = User.create({name: "Barbs", birthdate: "03-03-1994"})
-nutz = User.create({name: "Nutella", birthdate: "04-04-1995"})
 
-# EVENTS
-e1 = Event.create({content: "1. Tetris was released... ", conceptionDate: "05-12-1992"})
-e2 = Event.create({content: "1. Tetris was released... ", conceptionDate: "05-12-1993"})
-e3 = Event.create({content: "1. Tetris was released... ", conceptionDate: "05-12-1994"})
-e4 = Event.create({content: "1. Tetris was released... ", conceptionDate: "05-12-1995"})
-
-
-# COMMENTS
-c1 = Comment.create({content: "Crazy Kool", user_id: User.all.sample.id, event_id: Event.all.sample.id})
-c2 = Comment.create({content: "Crazy LALALAL", user_id: User.all.sample.id, event_id: Event.all.sample.id})
-c3 = Comment.create({content: "Crazy LOLOLOL", user_id: User.all.sample.id, event_id: Event.all.sample.id})
 
 
